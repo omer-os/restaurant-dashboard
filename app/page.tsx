35fa6f20054c63b906fab752d6a2386fc6cd6eef
omar chatin <omerchatin19@gmail.com>
2023-05-18 05:56:55 +0300

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import RestaurantInfoCard from "@components/templates/restaurantInfo/RestaurantInfoCard";
+import { auth, db } from "@lib/firebase";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div>
       <RestaurantInfoCard />
--- components/templates/restaurantInfo/RestaurantInfoCard.tsx
+++ components/templates/restaurantInfo/RestaurantInfoCard.tsx
@@ -25,89 +25,43 @@ export default function RestaurantInfoCard() {
   return (
     <div className="bg-white p-4 rounded shadow-lg flex flex-col md:max-w-md ">
       <h2 className="text-2xl font-bold mb-4 text-center">
-        {restaurant ? "Update Restaurant Profile" : "Create Restaurant Profile"}
+        Update Restaurant Profile
       </h2>
 
-      <div className="w-full mb-3 flex items-center justify-center">
-        <EditableImage
-          ImageComponent={<Avatar alt="inwero" size="xl" src={Image} />}
-          State={Image}
-          setState={setImage}
-        />
+
+      <div className="flex flex-col">
+
       </div>
-      {[
-        {
-          name: "Name",
-          value: Name,
-          setState: setName,
-        },
-        {
-          name: "Address",
-          value: Address,
-          setState: setAddress,
-        },
-        {
-          name: "Phone",
-          value: Phone,
-          setState: setPhone,
-        },
-        {
-          name: "Email",
-          value: Email,
-          setState: setEmail,
-        },
-      ].map((item) => (
-        <EditableListItem
-          key={item.name}
-          State={item.value}
-          setState={item.setState}
-          name={item.name}
-        />
-      ))}
-      <Button
-        // onClick={() => {
-        //   updateRestaurant(user.uid, {
-        //     Name,
-        //     Address,
-        //     Phone,
-        //     Email,
-        //     Image,
-        //   });
-        // }}
-        className="mt-4"
-      >
-        {restaurant ? "Update Restaurant Info" : "Create Restaurant Info"}
-      </Button>
     </div>
   );
 }
 
-const EditableListItem = ({
-  State,
-  setState,
-  name,
-}: {
-  State: string;
-  setState: any;
-  name: string;
-}) => {
-  const [Clicked, setClicked] = useState(false);
+// const EditableListItem = ({
+//   State,
+//   setState,
+//   name,
+// }: {
+//   State: string;
+//   setState: any;
+//   name: string;
+// }) => {
+//   const [Clicked, setClicked] = useState(false);
 
-  return (
-    <ListItem
-      pressable={false}
-      onClick={() => setClicked(true)}
-      onBlur={() => setClicked(false)}
-      className="mb-2"
-      intent="ghost"
-    >
-      <strong>{name}:</strong>
-      <EditableText
-        State={State}
-        setState={setState}
-        edit={Clicked}
-        onBlur={() => setClicked(false)}
-      />
-    </ListItem>
-  );
-};
+//   return (
+//     <ListItem
+//       pressable={false}
+//       onClick={() => setClicked(true)}
+//       onBlur={() => setClicked(false)}
+//       className="mb-2"
+//       intent="ghost"
+//     >
+//       <strong>{name}:</strong>
+//       <EditableText
+//         State={State}
+//         setState={setState}
+//         edit={Clicked}
+//         onBlur={() => setClicked(false)}
+//       />
+//     </ListItem>
+//   );
+// };
